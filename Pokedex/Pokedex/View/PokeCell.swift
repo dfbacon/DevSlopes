4601d023757df410b2f10cbe8e35bfb39b8dd774
Daniel Bacon <dbacon338@gmail.com>
2019-10-28 16:23:34 -0700

    Created custom cell class for collection view.

---
+++ Pokedex/Pokedex/View/PokeCell.swift
@@ -0,0 +1,25 @@
+//
+//  PokeCell.swift
+//  Pokedex
+//
+//  Created by Daniel Bacon on 10/28/19.
+//  Copyright © 2019 Daniel Bacon. All rights reserved.
+//
+
+import UIKit
+
+class PokeCell: UICollectionViewCell {
+    
+    @IBOutlet weak var thumbImage: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    var pokemon: Pokemon!
+    
+    func configureCell(pokemon: Pokemon) {
+        self.pokemon = pokemon
+        
+        nameLabel.text = self.pokemon.name
+        thumbImage.image = UIImage(named: "\(self.pokemon.pokedexID)")
+    }
+    
+}
