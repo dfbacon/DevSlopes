99cc76647aa7f3c3554ee2905907294a558bcde7
Daniel Bacon <dbacon338@gmail.com>
2019-10-28 16:40:26 -0700

    Updated view controller to handle collection view.

--- Pokedex/Pokedex/Controller/ViewController.swift
+++ Pokedex/Pokedex/Controller/ViewController.swift
@@ -8,10 +8,50 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout, UICollectionViewDataSource {
+    
+    @IBOutlet weak var collection: UICollectionView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        collection.dataSource = self
+        collection.delegate = self
+    }
+    
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 30
+    }
+    
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        if let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "PokeCell", for: indexPath) as? PokeCell {
+            
+            let pokemon = Pokemon(name: "Pokemon", pokedexID: indexPath.row)
+            cell.configureCell(pokemon: pokemon)
+            
+            return cell
+        }
+        else {
+            return UICollectionViewCell()
+        }
+    }
+    
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        <#code#>
+    }
+    
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        // same size set for the cell in the storyboard
+        return CGSize(width: 105, height: 105)
     }
 
 
--- Pokedex/Pokedex/View/PokeCell.swift
+++ Pokedex/Pokedex/View/PokeCell.swift
@@ -15,6 +15,13 @@ class PokeCell: UICollectionViewCell {
     
     var pokemon: Pokemon!
     
+    // Set rounded corners
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
+        layer.cornerRadius = 5.0
+    }
+    
     func configureCell(pokemon: Pokemon) {
         self.pokemon = pokemon
         
